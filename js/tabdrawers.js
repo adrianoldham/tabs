ab0341fe66974c326f252114b063f35ecc68da7a
Edmond Leung <leung.edmond@gmail.com>
2009-10-20 12:03:37 +0000

    Added ability to close drawers.

--- js/tabdrawers.js
+++ js/tabdrawers.js
@@ -1,6 +1,6 @@
 var TabDrawers = Class.create({
     initialize: function(tabsCollection, drawersCollection, options) {
-        this.options = Object.extend(Object.extend({ }, Tabs.DefaultOptions), options || { });
+        this.options = Object.extend(Object.extend({ }, TabDrawers.DefaultOptions), options || { });
         
         this.tabsCollection = tabsCollection;
         this.drawersCollection = drawersCollection;
@@ -11,7 +11,7 @@ var TabDrawers = Class.create({
                 this.drawersCollection.each(function(drawers) {
                     // Loop through wrapper, and find wrapper linked to menu
                     drawers.wrappers.each(function(wrapper) {
-                        new TabDrawers.Menu(menu, wrapper, drawers);
+                        new TabDrawers.Menu(menu, wrapper, drawers, this.options);
                     }.bind(this));
                 }.bind(this));                
             }.bind(this));
@@ -20,24 +20,38 @@ var TabDrawers = Class.create({
 });
 
 TabDrawers.Menu = Class.create({
-    initialize: function(menu, wrapper, drawers) {
+    initialize: function(menu, wrapper, drawers, options) {
         this.menu = menu;
         this.wrapper = wrapper;
         this.drawers = drawers;
+        this.options = options;
         
         // Loop through tabs and add click events if anchors are part of the wrapper
         this.menu.tabs.each(function(tab) {
             if (tab.anchor.ancestors().include(wrapper)) {
-                tab.anchor.observe("click", function(event) {
+                tab.anchor.observe("click", function(event, tab) {                    
                     // Since we assume only one drawers, then we grab the first tirgger
                     var trigger = this.drawers.triggers[0];
-                    this.drawers.toggleContent.call(this.drawers, trigger, false, true, true);
+                    var hide = false;
+                    
+                    // If closeDrawers is true, then check to see if we should close it
+                    if (this.options.closeDrawers) {
+                        if (this.lastActiveTab == tab) {
+                            hide = true;
+                            this.lastActiveTab = null;
+                        } else {
+                            this.lastActiveTab = tab;
+                        }
+                    }
+                    
+                    this.drawers.toggleContent.call(this.drawers, trigger, hide, true, true);
                     event.stop();
-                }.bindAsEventListener(this));
+                }.bindAsEventListener(this, tab));
             }
         }.bind(this));
     }
 });
 
 TabDrawers.DefaultOptions = {
+    closeDrawers: true
 };
