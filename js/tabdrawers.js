1f6eff963fa18c9cfeeaca71400c2cf5ad0d8c22
Adrian Oldham <adriann@visualthought.com.au>
2012-07-18 05:41:35 +0000

    Standardise formatting and whitespace.

--- js/tabdrawers.js
+++ js/tabdrawers.js
@@ -1,10 +1,10 @@
 var TabDrawers = Class.create({
     initialize: function(tabsCollection, drawersCollection, options) {
         this.options = Object.extend(Object.extend({ }, TabDrawers.DefaultOptions), options || { });
-        
+
         this.tabsCollection = tabsCollection;
         this.drawersCollection = drawersCollection;
-        
+
         this.tabsCollection.each(function(tabs) {
             tabs.menus.each(function(menu) {
                 // Find which drawers is attached to this menu
@@ -13,7 +13,7 @@ var TabDrawers = Class.create({
                     drawers.wrappers.each(function(wrapper) {
                         new TabDrawers.Menu(menu, wrapper, drawers, this.options);
                     }.bind(this));
-                }.bind(this));                
+                }.bind(this));
             }.bind(this));
         }.bind(this));
     }
@@ -25,15 +25,15 @@ TabDrawers.Menu = Class.create({
         this.wrapper = wrapper;
         this.drawers = drawers;
         this.options = options;
-        
+
         // Loop through tabs and add click events if anchors are part of the wrapper
         this.menu.tabs.each(function(tab) {
             if (tab.anchor.ancestors().include(wrapper)) {
-                tab.anchor.observe("click", function(event, tab) {                    
+                tab.anchor.observe("click", function(event, tab) {
                     // Since we assume only one drawers, then we grab the first tirgger
                     var trigger = this.drawers.triggers[0];
                     var hide = false;
-                    
+
                     // If closeDrawers is true, then check to see if we should close it
                     if (this.options.closeDrawers) {
                         if (this.lastActiveTab == tab) {
@@ -43,7 +43,7 @@ TabDrawers.Menu = Class.create({
                             this.lastActiveTab = tab;
                         }
                     }
-                    
+
                     this.drawers.toggleContent.call(this.drawers, trigger, hide, true, true);
                     event.stop();
                 }.bindAsEventListener(this, tab));
